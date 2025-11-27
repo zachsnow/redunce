59d46372d20fb3285a05d75429a1c645e117e953
Zach Snow <z@zachsnow.com>
2025-11-26 20:09:53 -0600

    Empty install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Install locally on MacOS.
+
+# 1. Check if homebrew is installed; if not error and bail.
+
+# 2. Check that the binary has been built; if not error and bail.
+
+# 3. Copy the binary to ~/.local/bin and the library to somewhere that's NOT in Homebrew but is in the default DYLD_LIBRARY_PATH.
+
+
+echo "Done."
