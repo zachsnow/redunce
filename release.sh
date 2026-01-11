1ebe20d6ef2c15c78fb6d6c9356a8eaf036d933e
Zach Snow <z@zachsnow.com>
2026-01-10 23:05:18 -0600

    Fix cleanup trap to return 0 on success.

--- release.sh
+++ release.sh
@@ -12,7 +12,8 @@ TAG="v$VERSION"
 
 # On failure: delete local tag, restore formula
 cleanup() {
-    [ $? -ne 0 ] || return
+    local code=$?
+    [ $code -ne 0 ] || return 0
     git tag -d "$TAG" 2>/dev/null
     (cd "$TAP_REPO" && git checkout -- Formula/redunce.rb 2>/dev/null)
 }
